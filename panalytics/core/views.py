00745630f75230905f2b519db6ed1d6866a184e4
Mike Smith <michael.eugene.smith@gmail.com>
2020-03-27 20:51:23 -0700

    Move JS string replacements into script view function

--- panalytics/core/views.py
+++ panalytics/core/views.py
@@ -8,17 +8,18 @@ from .utils import do_not_track
 JAVASCRIPT = """(function(){var w=window,d=document,
     i=new Image,e=encodeURIComponent;i.src='%s/a.gif?tid=%s&url='
     +e(d.location.href)+'&ref='+e(d.referrer)+'&t='+e(d.title)+'&wiw='
-    +e(w.innerWidth)+'&wih='+e(w.innerHeight);})()""".replace('\n', '').replace('    ', '')
+    +e(w.innerWidth)+'&wih='+e(w.innerHeight);})()"""
 
 # Transparent 1x1 GIF Pixel
 PXL = b64decode('R0lGODlhAQABAIAAANvf7wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==')
 
 
 def script(request):
+    clean_js = JAVASCRIPT.replace('\n', '').replace('    ', '')
     tid = request.GET.get('tid')
     if not Project.is_valid_tracking_id(tid):
         raise Http404
-    response = f'{JAVASCRIPT}' % (settings.ANALYTICS_HOST, tid)
+    response = f'{clean_js}' % (settings.ANALYTICS_HOST, tid)
     return HttpResponse(response, content_type="text/javascript")
 
 
